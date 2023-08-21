9ba5e16ccd92585fd20d653510945d423bc1e5a1
javiscripto <javier.mecker94@gmail.com>
2023-08-20 22:49:40 -0400

    modifico navBar.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { ItemListContainer } from './components/itemlistContainer/itemListContainer';
 import { ItemDetail } from './components/items/itemdetail';
-import NavBar from './components/nav/Navbar';
+import navBar from './components/nav/Navbar';
 import {  CustomProvider } from './cartContext/context';
 import { Carrito } from './components/cart/cart';
 import { Payment } from './components/payment/payment';
@@ -24,7 +24,7 @@ function App() {
       <CustomProvider>
      
       <BrowserRouter>
-        <NavBar/>
+        <navBar/>
         <main>
             <Routes>
               <Route path='/' element={<ItemListContainer/>} />
